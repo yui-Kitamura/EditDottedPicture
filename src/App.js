3ac94dd37b8753d19dbfdf122a20f27908b92ca6
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-12-20 16:02:13 +0900

    simple App style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section>
+        <input type={"file"} accept={"image/png"}/>
+      </section>
+      <figure>
+        <figcaption>入力画像</figcaption>
+          <picture>
+            <img id={"inputImg"} alt={"選択された画像"}/>
+          </picture>
+      </figure>
+      <figure>
+        <figcaption>出力画像</figcaption>
+        <img id={"outputImg"} alt={"出力された画像"} />
+      </figure>
+    </main>
   );
 }
 
