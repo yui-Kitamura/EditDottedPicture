db64507a77be2358fcddc5f22dfcaa1bebab7cad
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-12-21 15:13:42 +0900

    Implement function to upload and convert images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 
 function App() {
-  return (
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+        document.getElementById("inputImg").src = event.target.result;
+    };
+
+    function handleUpload() {
+        const selFiles = document.getElementById("selectInput").files;
+        if(!selFiles){
+            return false;
+        }
+        fileReader.readAsDataURL(selFiles[0]);
+    }
+
+    function handleConvert(){
+        //converter(inImgTag.src);
+    }
+
+    return (
     <main className="App">
       <section>
-        <input type={"file"} accept={"image/png"}/>
+        <input id={"selectInput"} type={"file"} accept={"image/png"} />
+        <input type={"button"} value={"upload"} onClick={handleUpload} />
+        <input type={"button"} value={"convert"} onClick={handleConvert} />
       </section>
       <figure>
         <figcaption>入力画像</figcaption>
