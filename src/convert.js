a7fa0dce8f90e43a4d4260d407cf9d9b897cf113
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-12-21 15:14:06 +0900

    update convert.js to be able to use with App

--- src/convert.js
+++ src/convert.js
@@ -1,13 +1,17 @@
 const sharp = require("sharp");
 
-sharp(process.argv[2])
-    .metadata().then(({ width }) => sharp(process.argv[2])
-    .resize(width * 8)
-    .toFile("cvt_" + process.argv[2]))
-.then(function(data){
-    console.log("Image converted successfully!");
-    console.dir(data);
-})
-.catch(function(err){
-    console.error(err);
-})
+const converter = function(input) {
+    sharp(input)
+        .metadata().then(({width}) => sharp(input)
+        .resize(width * 8)
+        .png())
+        .then(function (data) {
+            console.log("Image converted successfully!");
+            console.dir(data);
+        })
+        .catch(function (err) {
+            console.error(err);
+        })
+}
+
+export default converter;
