232ce1e286fe8cde4ba5603e89d7d00a77ad28c0
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-12-31 20:30:50 +0900

    React(node.js) -> Next.js replace

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"0yRn3WhqGIh5Ww2KWDtzgC3CnJyddKMgHWnViLAYN4Q=\"}"
---
+++ .next/static/chunks/app/layout-ba0a32a12daa959a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{308:function(e,n,_){Promise.resolve().then(_.t.bind(_,7844,23))},7844:function(e){e.exports={style:{fontFamily:"'__Inter_e66fe9', '__Inter_Fallback_e66fe9'",fontStyle:"normal"},className:"__className_e66fe9"}}},function(e){e.O(0,[971,938,744],function(){return e(e.s=308)}),_N_E=e.O()}]);
